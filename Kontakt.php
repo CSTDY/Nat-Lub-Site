1ff3fd2470b62d7395c2a2c5652b45a27dcd0c70
CSTDY <kamillsocha2000@gmail.com>
2020-03-23 19:11:50 +0100

    Basic site view

---
+++ Kontakt.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="static/css/main.css" type="text/css" />
+    <title>Portfolio</title>
+
+</head>
+
+<body>
+    <nav class="topnav">
+        <a href="index.php" >HOME</a>
+        <a href="Kontakt.php" style="background-color:rgba(167, 0, 22, 0.747); font-weight:bold;">Kontakt</a>
+        <a href="Prace.php">Prace</a>
+        <a href="Usługi.php">Usługi</a>
+        <a href="O-sobie.php">O sobie</a>
+    </nav>
+    <div class="container">
+        <div class="image">
+            <img src="static/images/hamster.jpg" alt="hamster">
+            <div class="content">
+                <a href="#">
+                    <h1>Heading</h1>
+                    <p>Projekt chomik</p>
+                </a>
+            </div>
+        </div>
+        <div class="image">
+            <img src="static/images/hamster.jpg" alt="hamster">
+            <div class="content">
+                <a href="#">
+                    <h1>Heading</h1>
+                    <p>Projekt chomik</p>
+                </a>
+            </div>
+        </div>
+        <div class="image">
+            <img src="static/images/hamster.jpg" alt="hamster">
+            <div class="content">
+                <a href="#">
+                    <h1>Heading</h1>
+                    <p>Projekt chomik</p>
+                </a>
+            </div>
+        </div>
+        <div class="image">
+            <img src="static/images/hamster.jpg" alt="hamster">
+            <div class="content">
+                <a href="#">
+                    <h1>Heading</h1>
+                    <p>Projekt chomik</p>
+                </a>
+            </div>
+        </div>
+        <div class="image">
+            <img src="static/images/hamster.jpg" alt="hamster">
+            <div class="content">
+                <a href="#">
+                    <h1>Heading</h1>
+                    <p>Projekt chomik</p>
+                </a>
+            </div>
+        </div>
+        <div class="image">
+            <img src="static/images/hamster.jpg" alt="hamster">
+            <div class="content">
+                <a href="#">
+                    <h1>Heading</h1>
+                    <p>Projekt chomik</p>
+                </a>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="static/css/main.css" type="text/css" />
+    <title>Portfolio</title>
+
+</head>
+
+<body>
+    <nav class="topnav">
+        <a href="index.php" style="background-color:rgba(167, 0, 22, 0.747); font-weight:bold;">HOME</a>
+        <a href="Kontakt.php">Kontakt</a>
+        <a href="Prace.php">Prace</a>
+        <a href="Usługi.php">Usługi</a>
+        <a href="O-sobie.php">O sobie</a>
+    </nav>
+    <section class="o-mnie">
+        <h1>Moje portfolio</h1>
+    </section>
+</body>
+
+</html>
