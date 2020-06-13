674333f9e5b0aff5f15c2432d6fb76b3291c62d9
Kamil Socha <47985744+CSTDY@users.noreply.github.com>
2020-06-13 10:42:09 +0200

    Update Head-section.php

--- Includes/Head-section.php
+++ Includes/Head-section.php
@@ -9,6 +9,6 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <link rel="stylesheet" href="static/cssIMG/main.css" type="text/css" />
-    <title>NatLubSite</title>
+    <title>Strona na zaliczenie</title>
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Pinyon+Script" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
